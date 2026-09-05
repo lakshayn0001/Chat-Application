ed329c3d400adc66b241fe059b285180a97877da
Lakshay <lakshay@Lakshays-MacBook-Air.local>
2026-09-05 17:56:27 +0530

    fix socket cors

--- socket-server.js
+++ socket-server.js
@@ -8,14 +8,25 @@ dotenv.config()
 
 const httpServer= createServer()
 
-const allowedOrigins= process.env.CLIENT_URL ? [process.env.CLIENT_URL,"http://localhost:3000"]:["http://localhost:3000","*"]
+const allowedOrigins = [
+    "http://localhost:3000",
+    "https://chat-application-blush-phi.vercel.app",
+    process.env.CLIENT_URL
+].filter(Boolean);
 
-const io= new Server(httpServer,{
-    cors:{
-        origin:allowedOrigins,
-        methods:["GET","POST"]
+const io = new Server(httpServer, {
+    cors: {
+        origin: (origin, callback) => {
+            
+            if (!origin || allowedOrigins.includes(origin) || origin.endsWith(".vercel.app")) {
+                callback(null, true);
+            } else {
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
+        methods: ["GET", "POST"]
     }
-})
+});
 
 io.on("connection",(socket)=>{
     console.log("New Connection",socket.id)
